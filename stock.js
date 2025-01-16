e759e71ee4a16cef20cc44311b3722c003e70884
tdavari <taha_davari@yahoo.com>
2025-01-16 10:28:28 +0330

    data_20250115

--- stock.js
+++ stock.js
@@ -12,10 +12,29 @@ $(document).ready(function () {
         { data: "chance" },
         { data: "market" },
         { data: "days_lock" },
+        { data: "buy_power" },
         { data: "vol_ratio" },
         { data: "group" },
       ],
       order: [[1, "desc"]],
     });
   });
+
+  // Fetch data from the API for the TSE descriptions table
+  const apiURL = "https://cdn.tsetmc.com/api/Msg/GetMsgByFlow/0/200";
+  $.getJSON(apiURL, function (response) {
+    // Filter messages to include only those with the specific phrase
+    const filteredMessages = response.msg.filter((msg) =>
+      msg.tseDesc.includes("با محدوديت دامنه نوسان قيمت بازگشايي") || msg.tseDesc.includes("گره")
+    );
+
+    // Initialize the DataTable with filtered data
+    $("#tseDescTable").DataTable({
+      data: filteredMessages,
+      columns: [
+        { data: "tseDesc" }, // Display only the tseDesc column
+      ],
+    });
+  });
+
 });
